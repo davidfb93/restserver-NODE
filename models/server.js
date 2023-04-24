6292dafd5362bd41d7629d7e3ecb81d7946c5661
David B <david.banol.3889@eam.edu.co>
2023-04-23 23:18:28 -0500

    configuracion a BD establecida

--- models/server.js
+++ models/server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const cors    = require('cors')
 
+const { dbConnection } = require('../database/config.db');
+
 class Server {
 
     constructor() {
@@ -9,11 +11,18 @@ class Server {
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
 
+        // Conectar a BD
+        this.conectarDB();
+
         // Middlewares
         this.middlewares();
 
         // Rutas de mi apicación
         this.routes(); 
+    }
+
+    async conectarDB() {
+        await dbConnection();
     }
 
     middlewares() {
