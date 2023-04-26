c57e493a40d1c59827dc8689e763e1d2744b7ef0
David B <david.banol.3889@eam.edu.co>
2023-04-26 18:03:20 -0500

    enlace con Cloudinary

--- controllers/uploads.controler.js
+++ controllers/uploads.controler.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const fs   = require('fs');
+
+const cloudinary = require('cloudinary').v2
+cloudinary.config( process.env.CLOUDINARY_URL );
+
 const { response } = require("express");
 
 const { Usuario, Producto } = require("../models");
@@ -18,7 +22,7 @@ const cargarArchivo = async (req, res = response) => {
     }
 }
 
-const actualizarImagen = async (req, res = response) => {
+/* const actualizarImagen = async (req, res = response) => {
 
     const { id, coleccion } = req.params;
 
@@ -59,10 +63,93 @@ const actualizarImagen = async (req, res = response) => {
     await modelo.save();
 
     res.json(modelo)
+} */
 
+const actualizarImagenCloudinary = async (req, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: `No existe un usuario con el id ${id}` });
+            }
+
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ msg: `No existe un producto con el id ${id}` });
+            }
+
+            break;
+        default:
+            return res.status(500).json({ msg: 'Sin validación actualmente' });
+    }
+
+    // Limpiar imágenes previas
+    if (modelo.img) {
+        
+    }
+
+    const { tempFilePath } = req.files.archivo;
+    const { secure_url } = await cloudinary.uploader.upload( tempFilePath )
+    modelo.img = secure_url;
+
+    await modelo.save();
+
+    res.json( modelo )
+}
+
+const mostrarImagen = async (req, res = response) => {
+
+    
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch (coleccion) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ 
+                    msg: `No existe un usuario con el id ${id}` 
+                });
+            }
+
+            break;
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if (!modelo) {
+                return res.status(400).json({ 
+                    msg: `No existe un producto con el id ${id}` 
+                });
+            }
+            break;
+        default:
+            return res.status(500).json({ 
+                msg: 'Sin validación actualmente' 
+            });
+    }
+
+    // Limpiar imágenes previas
+    if ( modelo.img ) {
+        // Borrar imagen previa
+        const pathImagen = path.join(__dirname, '../uploads', coleccion, modelo.img);
+        if ( fs.existsSync( pathImagen ) ){
+            return res.sendFile( pathImagen );
+        }
+    }
+
+    const pathImagen = path.join(__dirname, '../assets/no-image.jpg');
+    res.sendFile( pathImagen );
 }
 
 module.exports = {
     cargarArchivo,
-    actualizarImagen
+    actualizarImagenCloudinary,
+    mostrarImagen
 }
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos, validarArchivoSubir } = require('../middlewares');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads.controler');
+const { cargarArchivo, mostrarImagen, actualizarImagenCloudinary } = require('../controllers/uploads.controler');
 const { coleccionesPermitidas } = require('../helpers');
 
 const router = Router();
@@ -16,8 +16,13 @@ router.put('/:coleccion/:id', [
     check('id', 'El id debe ser de mongo').isMongoId(),
     check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'])),
     validarCampos
-], actualizarImagen )
+], actualizarImagenCloudinary )
 
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen )
 
 
 module.exports = router;
