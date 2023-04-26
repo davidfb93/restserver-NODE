fd56c052187c2a81604d6bfcd17b4f7ea3604382
David B <david.banol.3889@eam.edu.co>
2023-04-26 10:53:47 -0500

    busquedas finalizadas

--- controllers/buscar.controler.js
+++ controllers/buscar.controler.js
@@ -21,9 +21,59 @@ const buscarUsarios = async (termino = '', res = response) => {
         });
     }
 
+    const regex = new RegExp(termino, 'i');
 
-    
+    const usuarios = await Usuario.find({ 
+        $or: [{ nombre: regex }, { correo: regex }],
+        $and: [{ estado: true }]
+    });
+    res.json({
+        results: usuarios
+    })
 }
+
+const buscarCategorias = async (termino = '', res = response) => {
+
+    const esMongoID = isValidObjectId(termino); // true
+
+    if (esMongoID) {
+        const categoria = await Categoria.findById(termino);
+        return res.json({
+            results: (categoria) ? [categoria] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const categorias = await Categoria.find({ nombre: regex, estado: true });
+
+    res.json({
+        results: categorias
+    })
+}
+
+const buscarProductos = async (termino = '', res = response) => {
+
+    const esMongoID = isValidObjectId(termino); // true
+
+    if (esMongoID) {
+        const producto = await Producto.findById(termino)
+                                .populate('categoria', 'nombre');
+        return res.json({
+            results: (producto) ? [producto] : []
+        });
+    }
+
+    const regex = new RegExp(termino, 'i');
+
+    const productos = await Producto.find({ nombre: regex, estado: true })
+                                .populate('categoria', 'nombre');
+    
+    res.json({
+        results: productos
+    })
+}
+
 
 
 
@@ -40,11 +90,13 @@ const buscar = (req, res = response) => {
 
     switch ( coleccion ) {
         case 'usuarios':
-            buscarUsarios(termino, res)
+            buscarUsarios(termino, res);
             break;
         case 'categorias':
+            buscarCategorias(termino, res);
             break;
         case 'productos':
+            buscarProductos(termino, res);
             break;
         default:
             res.status(500).json({
