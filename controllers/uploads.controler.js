b70ccca86bce3dcbe4bd2ad604e35047803b5114
David B <david.banol.3889@eam.edu.co>
2023-04-26 12:00:59 -0500

    carga de archivos inicial

---
+++ controllers/uploads.controler.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { response } = require("express");
+
+const cargarArchivo = async (req, res = response) => {
+
+
+    if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
+        res.status(400).json({ msg: 'No files were uploaded.'});
+        return;
+    }
+
+    const { archivo } = req.files;
+
+    const uploadPath = path.join( __dirname , '../uploads/' , archivo.name);
+
+    archivo.mv(uploadPath,  (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+
+        res.json({ msg: 'File uploaded to ' + uploadPath });
+    });
+
+}
+
+module.exports = {
+    cargarArchivo
+}
--- models/server.js
+++ models/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const cors    = require('cors')
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config.db');
 
@@ -16,6 +17,7 @@ class Server {
             categorias: '/api/categorias',
             productos:  '/api/productos',
             usuarios:   '/api/usuarios',
+            uploads:    '/api/uploads',
         }
         
         // Conectar a BD
@@ -41,6 +43,12 @@ class Server {
 
         // Directorio público
         this.app.use( express.static('public'));
+
+        // FileUpload - Carga de archivos
+        this.app.use( fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/'
+        }));
     }
 
     routes() {
@@ -49,6 +57,7 @@ class Server {
         this.app.use( this.paths.categorias,require('../routes/categorias.routes'));
         this.app.use( this.paths.productos, require('../routes/productos.routes'));
         this.app.use( this.paths.usuarios,  require('../routes/usuarios.routes'));
+        this.app.use( this.paths.uploads,  require('../routes/uploads.routes'));
     }
 
     listen() {
